87773bd52349745e33edd00419723771478da55e
syedharishafeez <syed_haris98@yahoo.com>
2020-05-14 05:12:33 +0500

    Initial Commit

---
+++ square-using-string.py
@@ -0,0 +1,46 @@
+import math
+import collections
+import time
+
+inputString = input("Enter a string: ")
+start_time = time.time()
+
+def comparing_method(inputStringMethod, outputStringMethod):
+    if len(inputStringMethod) != len(outputStringMethod):
+        return False
+    for matchingIndex in range(0,len(inputStringMethod)):
+        if inputStringMethod[matchingIndex] != outputStringMethod[matchingIndex]:
+            return False
+    return True
+
+if len(inputString)>=1 and  len(inputString)<10:
+
+    inputStringResult = collections.Counter(inputString)
+    inputStringArray=[]
+    for value in inputStringResult.values():
+        inputStringArray.append(value)
+    inputStringArray.sort()
+
+    maximum_value = int(math.sqrt(10**len(inputString) - 1))
+    minimum_value = int(math.sqrt(10**(len(inputString)-1) - 1))
+
+    finalResult = -1
+    for i in range(maximum_value,minimum_value,-1):
+        outputStringResult = collections.Counter(str(i**2))
+        outputStringArray=[]
+        for value in outputStringResult.values():
+            outputStringArray.append(value)
+        match = False
+        matchingIndex = 0
+        if comparing_method(inputStringArray,outputStringArray):
+            finalResult = i**2
+            break
+        else:
+            continue
+
+    print("finalResult = "+str(finalResult))
+    
+else:
+    print("The length of input should be in between 0 - 10")
+
+print("--- %s seconds ---" % (time.time() - start_time))
